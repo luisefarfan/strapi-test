3fd9f94cb426fc179e2a15c53a5ca1e28dd72e98
LuisFarfan57 <luise.prado99@hotmail.com>
2024-10-01 23:00:33 -0600

    feat: add cloudflare r2 middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,37 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = ({ env }) => [
+  { name: 'strapi::logger' },
+  { name: 'strapi::errors' },
+  { name: 'strapi::cors' },
+  { name: 'strapi::poweredBy' },
+  { name: 'strapi::query' },
+  { name: 'strapi::body' },
+  { name: 'strapi::session' },
+  { name: 'strapi::favicon' },
+  { name: 'strapi::public' },
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            env("CF_PUBLIC_ACCESS_URL") ? env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, "") : "",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            env("CF_PUBLIC_ACCESS_URL") ? env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, "") : "",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  }
+]
